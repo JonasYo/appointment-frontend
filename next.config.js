bd4a853b87d3843de82aff41c00df5e66e5f26ec
Jonas Siqueira Marcelino <jonascska07@gmail.com>
2021-08-18 21:31:20 -0300

    build: fix error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withImages = require('next-images');
-const optimizedImages = require('next-optimized-images');
 
 module.exports = withImages({
   target: 'serverless',
   cssModules: true,
   esModule: true,
-  optimizedImages,
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
 });
